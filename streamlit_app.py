8e0678275ab06bf46350e27a96d2741477918118
HoogendoornM <36399923+HoogendoornM@users.noreply.github.com>
2022-07-01 11:01:00 +0200

    added selection option

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Load fruit CSV
 df = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-# Display fruit df
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(df.index))
+
+# Display the table on the page.
 streamlit.dataframe(df)

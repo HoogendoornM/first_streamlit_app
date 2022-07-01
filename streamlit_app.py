71d22d7c02481876d359babeac3c4639cc725dde
HoogendoornM <36399923+HoogendoornM@users.noreply.github.com>
2022-07-01 11:01:54 +0200

    changed selection index to fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 df = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(df.index))
+streamlit.multiselect("Pick some fruits:", list(df.fruit))
 
 # Display the table on the page.
 streamlit.dataframe(df)

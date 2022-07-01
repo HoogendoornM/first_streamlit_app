14c6443aae7fda818dfadc50943f98a0e2ee92d4
HoogendoornM <36399923+HoogendoornM@users.noreply.github.com>
2022-07-01 10:55:06 +0200

    added pandas, imported fruit file and added streamlit dataframe view to page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('Streamlit tutorial')
 
@@ -8,3 +9,9 @@ streamlit.text('🍊 Orange')
 streamlit.text('🍔 Beefburger')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Load fruit CSV
+df = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Display fruit df
+streamlit.dataframe(df)

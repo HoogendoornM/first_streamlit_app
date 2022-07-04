5f13d357e62c50b40f0678ce1995860ba5b64f23
HoogendoornM <36399923+HoogendoornM@users.noreply.github.com>
2022-07-04 11:45:21 +0200

    trying python string format

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,4 @@ streamlit.header("Fruit list:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like add?')
+my_cur.execute("INSERT INTO fruit_load_list (FRUIT_NAME) VALUES ({});".format(add_my_fruit))
